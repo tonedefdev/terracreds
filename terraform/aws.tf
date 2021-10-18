19dac3bb798de92cb6c9a0e33236b1e8477d4734
Tone Def <tonedefdev@gmail.com>
2021-10-17 23:43:46 -0700

    Added the initial terraform resources to perform end-to-end tests on aws

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -1,1 +1,60 @@
-//TODO: Add resources for AWS terracreds testing
+resource "aws_vpc" "vpc" {
+  count      = var.test_aws ? 1 : 0
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "vm_subnet" {
+  count             = var.test_aws ? 1 : 0
+  availability_zone = "us-west-2a"
+  vpc_id            = aws_vpc.vpc[count.index].id
+  cidr_block        = "10.0.1.0/24"
+
+  tags = {
+    Name = "vm_subnet"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  count       = var.test_aws ? 1 : 0
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.vpc[count.index].id
+
+  ingress = [
+    {
+      description      = "SSH from Internet"
+      cidr_blocks      = ["0.0.0.0/0"]
+      from_port        = 22
+      to_port          = 22
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+    }
+  ]
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+resource "aws_key_pair" "test_vm_keys" {
+  count      = var.test_aws ? 1 : 0
+  key_name   = "test_vm_keys"
+  public_key = file("C:\\Temp\\aws_rsa.pub")
+}
+
+resource "aws_instance" "test_vm" {
+  count                       = var.test_aws ? 1 : 0
+  ami                         = "ami-005e54dee72cc1d00"
+  associate_public_ip_address = true
+  availability_zone           = "us-west-2a"
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.test_vm_keys[count.index].key_name
+  subnet_id                   = aws_subnet.vm_subnet[0].id
+
+  vpc_security_group_ids = [
+    aws_security_group.allow_ssh[0].id
+  ]
+}
--- terraform/azure.tf
+++ terraform/azure.tf
@@ -1,6 +1,7 @@
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "rg" {
+  count    = var.test_az ? 1 : 0
   name     = var.rg_name
   location = var.location
 }
@@ -8,8 +9,8 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_public_ip" "pip" {
   count               = var.test_az ? 1 : 0
   name                = "terracreds-test-pip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg[0].location
+  resource_group_name = azurerm_resource_group.rg[0].name
   allocation_method   = "Dynamic"
 }
 
@@ -17,14 +18,14 @@ resource "azurerm_virtual_network" "vnet" {
   count               = var.test_az ? 1 : 0
   name                = "${var.vm_name}-vnet"
   address_space       = ["10.1.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg[0].location
+  resource_group_name = azurerm_resource_group.rg[0].name
 }
 
 resource "azurerm_subnet" "subnet" {
   count                = var.test_az ? 1 : 0
   name                 = "${var.vm_name}-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.rg[0].name
   virtual_network_name = azurerm_virtual_network.vnet[count.index].name
   address_prefixes     = ["10.1.0.0/24"]
 }
@@ -32,8 +33,8 @@ resource "azurerm_subnet" "subnet" {
 resource "azurerm_network_interface" "nic" {
   count               = var.test_az ? 1 : 0
   name                = "${var.vm_name}-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg[0].location
+  resource_group_name = azurerm_resource_group.rg[0].name
 
   ip_configuration {
     name                          = "external"
@@ -46,8 +47,8 @@ resource "azurerm_network_interface" "nic" {
 resource "azurerm_windows_virtual_machine" "vm" {
   count               = var.test_az ? 1 : 0
   name                = var.vm_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg[0].location
+  resource_group_name = azurerm_resource_group.rg[0].name
   size                = var.vm_size
   admin_username      = "terraadmin"
   admin_password      = "TerracredsR0cks!" // Yes, I know this is bad form, but it's only a test machine :P
@@ -75,8 +76,8 @@ resource "azurerm_windows_virtual_machine" "vm" {
 resource "azurerm_key_vault" "keyvault" {
   count                       = var.test_az ? 1 : 0
   name                        = var.keyvault_name
-  location                    = azurerm_resource_group.rg.location
-  resource_group_name         = azurerm_resource_group.rg.name
+  location                    = azurerm_resource_group.rg[0].location
+  resource_group_name         = azurerm_resource_group.rg[0].name
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
