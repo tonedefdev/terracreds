a6be8658e3cbdf54c2459f136c7c0929bf4b8f76
Tony Owens <anthony.owens1987@gmail.com>
2023-02-04 20:57:19 -0800

    Fixed test to check for os version to verify correct config file name is checked

--- cmd/generate_test.go
+++ cmd/generate_test.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"os"
 	"path/filepath"
+	"runtime"
 	"testing"
 
 	"github.com/urfave/cli/v2"
@@ -31,8 +32,15 @@ func TestCreateConfig(t *testing.T) {
 	args = append(args, "generate", "--create-cli-config", "--force")
 	app.Run(args)
 
+	var configFileName string
+	if runtime.GOOS == "darwin" || runtime.GOOS == "linux" {
+		configFileName = ".terraformrc"
+	} else {
+		configFileName = "terraform.rc"
+	}
+
 	userProfile := os.Getenv("HOME")
-	cliConfig := filepath.Join(userProfile, ".terraformrc")
+	cliConfig := filepath.Join(userProfile, configFileName)
 
 	_, err := os.ReadFile(cliConfig)
 	if err != nil {
